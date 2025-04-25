b886829edd361c4ee0b536419b0ef9287537e7f8
Nicolas Mil-Homens Cavaco <79808450+mhd1900@users.noreply.github.com>
2025-04-26 00:28:01 +0200

    Update inject_postprocess_pca.sh

--- inject_postprocess_pca.sh
+++ inject_postprocess_pca.sh
@@ -3,6 +3,6 @@ for flux in 0.0 0.25 0.5
 do
 for seed in 4 5 6
 do
-srun python3 main.py "outputs_pca_grid_24_30/" 1 pca "30,0" "None,None,2" "None,None,4" 120 "4,7" $flux $seed "5,12" "empty_cubes/" 2
+srun python3 main.py "outputs_pca/" 1 pca "30,0" "None,None,2" "None,None,4" 120 "4,7" $flux $seed "5,12" "empty_cubes/" 2
 done
 done
