55444169bb83e3987d14d44f2fd1e18090ff3312
Nicolas Mil-Homens Cavaco <79808450+mhd1900@users.noreply.github.com>
2025-04-26 00:27:40 +0200

    Update inject_postprocess_fista.sh

--- inject_postprocess_fista.sh
+++ inject_postprocess_fista.sh
@@ -5,7 +5,7 @@ for seed in 4 5 6
 do
 for alpha in 0.0 2.0
 do
-python3 main.py "outputs_fista_grid_24_40_$alpha/" 1 fista "40,$alpha" "None,None,2" "None,None,4" 120 "4,7" $flux $seed "5,12" "empty_cubes/" 2
+python3 main.py "outputs_fista_$alpha/" 1 fista "40,$alpha" "None,None,2" "None,None,4" 120 "4,7" $flux $seed "5,12" "empty_cubes/" 2
 done
 done
 done
