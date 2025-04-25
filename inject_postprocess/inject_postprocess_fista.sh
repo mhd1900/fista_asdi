fe0fae2256e449ec7b0c0a6e2cdd197870fade1b
Nicolas Mil-Homens Cavaco <79808450+mhd1900@users.noreply.github.com>
2025-04-26 00:48:31 +0200

    Add files via upload

---
+++ inject_postprocess/inject_postprocess_fista.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for flux in 0.0 0.25 0.5
+do
+for seed in 4 5 6
+do
+for alpha in 0.0 2.0
+do
+python3 main.py "outputs_fista_$alpha/" 1 fista "40,$alpha" "None,None,2" "None,None,4" 120 "4,7" $flux $seed "5,12" "empty_cubes/" 2
+done
+done
+done
